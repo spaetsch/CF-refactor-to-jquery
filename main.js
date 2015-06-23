792aa15aa0cdbb8c291cc853d595c7c5581ca710
Sarah Paetsch <spaetsch@gmail.com>
2015-06-23 13:03:16 -0700

    Convert submit message to jQuery

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // function verifyEmail() {
 //   var status= document.getElementById('emailStatus');
-
 //   var emailPattern=/^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
 //   if (emailPattern.test(subscribeField.value)) {
 //     status.classList.remove("notApproved");
@@ -10,15 +9,11 @@
 //     status.classList.add("notApproved");
 //   }
 // }
-
 // var elEmail = document.getElementById('subscribeField')
 // elEmail.addEventListener('keyup', verifyEmail, false);
 
-
 $("#subscribeField").keyup(function(){
   var emailPattern = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
-  console.log("invoked keyup");
-  console.log("emailPattern.test(subscribeField.value)", emailPattern.test(subscribeField.value));
 
   if (emailPattern.test(subscribeField.value)) {
     $("#emailStatus").removeClass("notApproved");
@@ -29,16 +24,24 @@ $("#subscribeField").keyup(function(){
   }
 });
 
-function submitMessage (e) {
-  e.preventDefault();
-  var submitMsg = document.getElementById('message');
-  submitMsg.textContent = 'Thank you. We will now sell your email to spammers.';
-}
+// function submitMessage (e) {
+//   e.preventDefault();
+//   var submitMsg = document.getElementById('message');
+//   submitMsg.textContent = 'Thank you. We will now sell your email to spammers.';
+// }
+//var submitButton = document.getElementById('submit')
+// submitButton.addEventListener('click', submitMessage, false);
+
+$("#submit").click(function(event){
+  console.log("invoked");
+  event.preventDefault();
+  $("#message").text('Thank you. We will now sell your email to spammers.');
+});
+
+
 function changeColor () {
   form.classList.add("completed")
 }
-var submitButton = document.getElementById('submit')
 var form = document.getElementById('formsection')
-submitButton.addEventListener('click', submitMessage, false);
 form.addEventListener('click', changeColor, false)
 
